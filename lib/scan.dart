97d6f69df21e06e6aa3391ce1768f1e008452a2b
lamdiv <imdiwash.mrprogrammer@gmail.com>
2022-06-20 14:50:58 +0545

    added ui to scan screen

--- lib/scan.dart
+++ lib/scan.dart
@@ -38,37 +38,99 @@ class _ScanState extends State<Scan> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        child: (Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Scan The Snake',
-              style: TextStyle(
-                  color: Color(0xFF323232),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              child: FutureBuilder<void>(
-                future: _initializeControllerFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    // If the Future is complete, display the preview.
-                    return CameraPreview(_controller);
-                  } else {
-                    // Otherwise, display a loading indicator.
-                    return const Center(
-                        child: CircularProgressIndicator(
-                      color: Color(0xFF2E5043),
-                    ));
-                  }
-                },
-              ),
-            )
-          ],
-        )));
+    return Container(
+      width: double.infinity,
+      height: double.maxFinite,
+      // height: MediaQuery.of(context).size.height - 00,
+      child: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(
+              _controller,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      children: const [
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Scan The Snake',
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 8.0, right: 15),
+                            child: Icon(
+                              Icons.flash_off,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          const Icon(
+                            Icons.image,
+                            size: 33,
+                            color: Colors.white,
+                          ),
+                          Icon(
+                            Icons.circle,
+                            size: 55,
+                            color: Colors.white,
+                            shadows: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.7),
+                                spreadRadius: 25,
+                                blurRadius: 20,
+
+                                // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor: const Color(0xFF2E5043),
+                              minimumSize: Size(80, 33),
+                              padding: EdgeInsets.symmetric(horizontal: 16.0),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50.0)),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text('Predict'),
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+            );
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(
+                child: CircularProgressIndicator(
+              color: Color(0xFF2E5043),
+            ));
+          }
+        },
+      ),
+    );
   }
 }
